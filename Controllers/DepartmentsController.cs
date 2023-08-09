755db6dd0c43699fcd6e9621608a5e8b6908476e
Jack Liu Shurui - TO <jliusr@ntuc.sg>
2023-08-09 19:18:48 +0800

    advanced scenarios

--- Controllers/DepartmentsController.cs
+++ Controllers/DepartmentsController.cs
@@ -34,11 +34,12 @@ namespace ContosoUniversity.Controllers
             {
                 return NotFound();
             }
-
+            string query = "SELECT * FROM Department WHERE DepartmentID = {0}";
             var department = await _context.Departments
+                .FromSqlRaw(query, id)
                 .Include(d => d.Administrator)
                 .AsNoTracking()
-                .FirstOrDefaultAsync(m => m.DepartmentID == id);
+                .FirstOrDefaultAsync();
             if (department == null)
             {
                 return NotFound();
